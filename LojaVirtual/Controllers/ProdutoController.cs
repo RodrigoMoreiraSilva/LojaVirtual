ee499194fd4c937d3317bc7fa87fea70a50c2281
Rodrigo Moreira Silva <rodrigo.silva@damasio.edu.br>
2021-02-22 17:46:06 -0300

    Criando as páginas index e contato

--- LojaVirtual/Controllers/ProdutoController.cs
+++ LojaVirtual/Controllers/ProdutoController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LojaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,8 +11,23 @@ namespace LojaVirtual.Controllers
     {
         public ActionResult Visualizar()
         {
+            Produto produto = GetProduto();
 
-            return new ContentResult() { Content = "<h3>Produto -> Visualizar</h3>", ContentType = "Text/html" };
+            return View(produto);
+
+            //return new ContentResult() { Content = "<h3>Produto -> Visualizar</h3>", ContentType = "Text/html" };
         }
+
+        private Produto GetProduto()
+        {
+            return new Produto
+            {
+                Id = 1,
+                Nome = "Xbox One X",
+                Descricao = "Jogue em 4K",
+                Valor = 2000.00M
+            };
+        }
+
     }
 }
