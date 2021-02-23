8a127f10a566e6cb4476426367774a8f89e253ff
Rodrigo Moreira Silva <rodrigo.silva@damasio.edu.br>
2021-02-23 15:56:58 -0300

    Configurando envio de e-mail de contato.

---
+++ LojaVirtual/Libraries/Email/ContatoEmail.cs
@@ -0,0 +1,43 @@
+﻿using LojaVirtual.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Libraries.Email
+{
+    public class ContatoEmail
+    {
+        public static void EnviarContatoPorEmail(Contato contato)
+        {
+            //SMTP
+            SmtpClient smtp = new SmtpClient("smtp.office365.com", 587);
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("appsiga@damasio.edu.br", "*****");
+            smtp.EnableSsl = true;
+
+            //Corpo do E-mail
+
+            string corpoMsg = string.Format("<h2>Contato - Loja Virtual</h2></br>" +
+                "<b>Nome: </b>{0}</br>" +
+                "<b>E-mail: </b>{1}</br>" +
+                "<b>Texto: </b>{2}</br>" +
+                "E-mail enviado automaticamente do site LojaVirtual.",
+                contato.Nome,contato.Email,contato.Texto
+                );
+
+            MailMessage mensagem = new MailMessage();
+
+            mensagem.From = new MailAddress("appsiga@damasio.edu.br");
+            mensagem.To.Add(new MailAddress(contato.Email));
+            mensagem.Subject = "Contato Loja Virtual - E-mail: " + contato.Email;
+            mensagem.Body = corpoMsg;
+            mensagem.IsBodyHtml = true;
+
+            //Enviar mensagem
+            smtp.Send(mensagem);
+        }
+    }
+}
