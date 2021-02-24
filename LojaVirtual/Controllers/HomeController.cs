a014c5cff982ba523cdabc4ad6f6e1df88176a07
Rodrigo Moreira Silva <rodrigo.silva@damasio.edu.br>
2021-02-24 11:08:20 -0300

    Configurando validação com arquivo de recursos

--- LojaVirtual/Controllers/HomeController.cs
+++ LojaVirtual/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using LojaVirtual.Libraries.Email;
 using LojaVirtual.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
 
 namespace LojaVirtual.Controllers
 {
@@ -22,16 +24,49 @@ namespace LojaVirtual.Controllers
 
         public IActionResult ContatoAcao()
         {
+            try
+            {
+                Contato contato = new Contato();
+                contato.Nome = HttpContext.Request.Form["nome"];
+                contato.Email = HttpContext.Request.Form["email"];
+                contato.Texto = HttpContext.Request.Form["texto"];
 
-            Contato contato = new Contato();
-            
-            contato.Nome = HttpContext.Request.Form["nome"];
-            contato.Email = HttpContext.Request.Form["email"];
-            contato.Texto = HttpContext.Request.Form["texto"];
+                var listaMensagens = new List<ValidationResult>();
+                var contexto = new ValidationContext(contato);
+                bool isValid = Validator.TryValidateObject(contato, contexto, listaMensagens, true);
 
-            ContatoEmail.EnviarContatoPorEmail(contato);
+                if (isValid)
+                {
+                    ContatoEmail.EnviarContatoPorEmail(contato);
 
-            return new ContentResult() { Content = String.Format("Dados recebidos com sucesso! </br>Nome: {0}</br>E-mail: {1}</br>Texto: {2}",contato.Nome,contato.Email,contato.Texto), ContentType="text/html" };
+                    ViewData["MSG_S"] = "Mensagem de contato enviado com sucesso!";
+                }
+                else
+                {
+                    StringBuilder sb = new StringBuilder();
+                    foreach(var texto in listaMensagens)
+                    {
+                        sb.Append(texto.ErrorMessage + "<br />");
+                    }
+
+                    ViewData["MSG_E"] = sb.ToString();
+                }
+
+            }
+            catch (Exception e)
+            {
+                ViewData["MSG_E"] = "Opps! Tivemos um erro, tente novamente mais tarde!";
+
+                //TODO - Implementar log
+
+            }
+
+
+
+
+            return View("Contato");
+
+            //return new ContentResult() { Content = String.Format("Dados recebidos com sucesso! </br>Nome: {0}</br>E-mail: {1}</br>Texto: {2}",contato.Nome,contato.Email,contato.Texto), ContentType="text/html" };
         }
 
         public IActionResult Login()
