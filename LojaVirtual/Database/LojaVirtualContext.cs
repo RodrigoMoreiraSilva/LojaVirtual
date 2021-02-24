0e15f0c144a36060afb8f883335b8c9b22c6e8ad
Rodrigo Moreira Silva <rodrigo.silva@damasio.edu.br>
2021-02-24 14:23:49 -0300

    Criando o contexto de Clientes para EF.

--- LojaVirtual/Database/LojaVirtualContext.cs
+++ LojaVirtual/Database/LojaVirtualContext.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using LojaVirtual.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace LojaVirtual.Database
 {
-    public class LojaVirtualContext
+    public class LojaVirtualContext : DbContext
     {
+        public LojaVirtualContext(DbContextOptions<LojaVirtualContext>options) 
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
     }
 }
